13ce19a293acefe034117303a67b9e57f1a52873
arthurjf <arthur.310100@hotmail.com>
2023-03-28 12:40:45 -0300

    Checagem das regras do calculo

--- app/src/main/java/br/com/arthurjf/cursoandroid/calculadoraimc/view/MainActivity.kt
+++ app/src/main/java/br/com/arthurjf/cursoandroid/calculadoraimc/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package br.com.arthurjf.cursoandroid.calculadoraimc.view
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -21,12 +22,43 @@ class MainActivity : AppCompatActivity() {
         val editTextWeight = findViewById<EditText>(R.id.editTextWeight)
 
         buttonCalculate.setOnClickListener {
-            val calculoTeste =
-                editTextWeight.text.toString().toDouble() / (editTextHeight.text.toString()
-                    .toDouble().pow(2))
+            var isValuesOk = true
 
-            val roundOff = String.format(Locale.US, "%.1f", calculoTeste)
-            Toast.makeText(this, "Seu IMC: $roundOff", Toast.LENGTH_LONG).show()
+            if (TextUtils.isEmpty(editTextWeight.text)) {
+                editTextWeight.error = "Insira o seu peso"
+                isValuesOk = false
+            }
+
+            if (TextUtils.isEmpty(editTextHeight.text)) {
+                editTextHeight.error = "Insira a sua altura"
+                isValuesOk = false
+            }
+
+            if (isValuesOk) {
+                val height = editTextHeight.text.toString().toDouble()
+                val weight = editTextWeight.text.toString().toDouble()
+
+                if (weight <= 0) {
+                    editTextWeight.error = "Peso não permitido"
+                    isValuesOk = false
+                }
+
+                if (height <= 0) {
+                    editTextHeight.error = "Altura não permitida"
+                    isValuesOk = false
+                }
+
+                if (isValuesOk) {
+                    val imc = weight / (height.pow(2))
+
+                    val formattedResult = String.format(Locale.US, "%.1f", imc)
+
+                    Toast.makeText(this, "Seu IMC: $formattedResult", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(this, "Por favor, corrija os dados informados", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
 
         buttonClear.setOnClickListener {
